836934fc706a0814b94b13ad849e778b03c17220
muhammadafriasnyach <muhamadafriansyach123@gmail.com>
2021-04-04 21:03:42 -0700

    coding pertama

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>kalkulator sederhana</title>
+	</head>
+	<body>
+		<form action="proses.php" method="post">
+			<input type="text" name="angka1">
+			<input type="text" name="angka2">
+			<input type="submit" name="tambah" value="tambah">
+		</form>
+
+	</body>
+</html>
